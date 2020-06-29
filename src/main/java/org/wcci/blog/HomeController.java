a63ddc9a030cc37bd51f2094dabbb55d049b4357
Christin Banerjee <Christinprice5@gmail.com>
2020-06-28 20:03:09 -0400

    push changes

--- src/main/java/org/wcci/blog/HomeController.java
+++ src/main/java/org/wcci/blog/HomeController.java
@@ -2,16 +2,27 @@ package org.wcci.blog;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-    private final CategoryStorage campusStorage;
-    private final AuthorStorage authorStorage;
 
-    public HomeController(CategoryStorage campusStorage, AuthorStorage authorStorage) {
-        this.campusStorage = campusStorage;
-        this.authorStorage = authorStorage;
+    private final HashtagRepository hashtagRepo;
+    private final CategoryRepository categoryRepo;
+    private final AuthorRepository authorRepo;
+
+
+    public HomeController(HashtagRepository hashtagRepo, CategoryRepository categoryRepo, AuthorRepository authorRepo) {
+        this.hashtagRepo = hashtagRepo;
+        this.categoryRepo = categoryRepo;
+        this.authorRepo = authorRepo;
     }
 
+    @GetMapping({"", "/"})
+    public String Home(Model model) {
+        model.addAttribute("categories",categoryRepo.findAll());
+        model.addAttribute("hashtags", hashtagRepo.findAll());
+        model.addAttribute("authors", authorRepo.findAll());
+        return "home-template";
+    }
 }
